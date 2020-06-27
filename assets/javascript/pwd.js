1e6283ad8971c0a81a2b4eed090e280a22a796d5
pratyu <pratyusha.raghupatruni@gmail.com>
2020-06-27 02:00:55 -0700

    few changes made in css

--- assets/javascript/pwd.js
+++ assets/javascript/pwd.js
@@ -4,7 +4,7 @@ var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}'];
-var temp = '';
+
 
 window.onload = alert("Welcome! Please click 'Generate password' to start!");
 
@@ -12,11 +12,11 @@ function generatePassword() {
     var length = prompt("enter the password length");
     console.log(length);
 
-    if (length < 6 || length > 56) {
+    if (length < 6 || length >= 128) {
         alert("The length of the password should be atleast 6 and less than 128 characters" + length);
     }
 
-    else if (length > 6 || length < 56) {
+    else if (length >= 6 || length < 128) {
         var upperCaseEl = confirm("do you want upper case letters");
         var lowerCaseEl = confirm("do you want to have lowercase letters");
         var numbersEl = confirm("do you want to have numbers");
@@ -44,23 +44,24 @@ function generatePassword() {
     if (possibleChar.length === 0) {
         alert("you should enter atleast 1 type of characters");
     }
- 
-        for (var i = 0; i < length; i++) {
-            var getRandom = Math.floor(Math.random() * possibleChar.length);
-            resultPassword = resultPassword + getRandom[possibleChar];
-        }
-        console.log(resultPassword);
-    
+
+    for (var i = 0; i < length; i++) {
+        var getRandom = Math.floor(Math.random() * possibleChar.length);
+        resultPassword = resultPassword + possibleChar[getRandom];
+    }
+    console.log(resultPassword);
+
     document.getElementById("pwd").innerHTML = resultPassword;
 }
 
 function copyClipboard() {
-  document.querySelector("h4").select();
 
+    var copyText = document.getElementById("pwd");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
 
-    /* Copy the text inside the text field */
     document.execCommand("copy");
-
+    alert("Copied the text: " + copyText.value);
     /* Alert the copied text */
-    alert("Copied the text: " );
+    //  alert("Copied the text: " );
 }
