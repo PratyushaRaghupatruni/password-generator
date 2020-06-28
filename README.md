2902742818c1909ec90519d3c057df1e5705e3de
pratyu <pratyusha.raghupatruni@gmail.com>
2020-06-28 15:54:34 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # password-generator
+
+Description:This application generates a random password based on user-selected criteria. The application validates user input and ensure that at least one character type is selected. Once all prompts are answered, the user will be presented with a password matching the answered prompts. This application is also responsive, ensuring that it adapts to multiple screen sizes.
+
+Deployed link:https://pratyusharaghupatruni.github.io/password-generator/
+
+Usage:
+Generating a Random Password when the user clicks generate Password.This is built using Javascript.
+
+![alt text]
+
+Instructions
+Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. The user will be prompted to choose from the following password criteria:
+
+Length (must be between 8 and 128 characters)
+Character type:
+Uppercase characters
+Lowercase characters
+Special characters
+Numeric characters
+
+
+Business context
+For companies that handle large amounts of sensitive data, weak passwords can pose a real security threat. An application that can generate strong passwords quickly and effortlessly saves employees time and ensures secure access to data.
+
+Bonus
+As a bonus, the user should also have the option to click a button to copy the password to their clipboard.
--- assets/javascript/pwd.js
+++ assets/javascript/pwd.js
@@ -1,47 +1,56 @@
+
+//array of the uppercase
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//array of lowercase
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//array of numbers
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+//array of symbols
 var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}'];
 
+//function to generate random when clicking generate password
 function generatePassword() {
+    //entering the length of the password the user wants
     var length = prompt("enter the password length");
-    console.log(length);
+    //An empty Array
+    var possibleChar = [];
+    //The result of the password intially it is empty
+    var resultPassword = "";
 
+    //condition for checking the password length minimum 8 and max 128
     if (length < 8 || length >= 128) {
         alert("The length of the password should be atleast 8 and less than 128 characters");
+        resultPassword = "Not a valid password!";
     }
-
     else if (length >= 8 || length < 128) {
+        //user requirement conditions
         var upperCaseEl = confirm("do you want upper case letters");
         var lowerCaseEl = confirm("do you want to have lowercase letters");
         var numbersEl = confirm("do you want to have numbers");
         var symbolsEl = confirm("do you want to have symbols");
-
-        var possibleChar = [];
-        var resultPassword = "";
-
+        //if uppercase is true push the elements to the array possible char
         if (upperCaseEl) {
             Array.prototype.push.apply(possibleChar, upperCase);
         }
-        console.log(possibleChar);
+        //if lowercase is true push the elements to the array possible char       
         if (lowerCaseEl) {
             Array.prototype.push.apply(possibleChar, lowerCase);
         }
-        console.log(possibleChar);
+        //if numbers is true push the elements to the array possible char       
         if (numbersEl) {
             Array.prototype.push.apply(possibleChar, numbers);
         }
-        console.log(possibleChar);
+        //if symbols is true push the elements to the array possible char    
         if (symbolsEl) {
             Array.prototype.push.apply(possibleChar, symbols);
         }
-        console.log(possibleChar);
-
+        //if none of them is true 
         if (possibleChar.length === 0) {
             alert("you should enter atleast 1 type of characters");
         }
+        //if any of the above is true theen generating a random pwd    
         else {
             for (var i = 0; i < length; i++) {
                 var getRandom = possibleChar[Math.floor(Math.random() * possibleChar.length)];
@@ -49,19 +58,19 @@ function generatePassword() {
             }
         }
     }
-    console.log(resultPassword);
-
+    //generating the password to the textarea on th ewindow
     document.getElementById("pwd").innerHTML = resultPassword;
+    //enabling the copy clipboard after password is generated to copy   
+    document.getElementById("copyClipboard").disabled = false;
 }
-
+//function for copying the password to the clipboard
 function copyClipboard() {
-
     var copyText = document.getElementById("pwd");
+    //coying the generated password
     copyText.select();
     copyText.setSelectionRange(0, 99999);
-
+    //command to the cpy pwd
     document.execCommand("copy");
+    //An alert that the text is copied
     alert("Copied the text: " + copyText.value);
-    /* Alert the copied text */
-    //  alert("Copied the text: " );
 }
