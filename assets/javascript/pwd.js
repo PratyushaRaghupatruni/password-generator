0731c3d76db50e015477d253a4706dbc393a819d
pratyu <pratyusha.raghupatruni@gmail.com>
2020-06-27 17:12:18 -0700

    js validation

--- assets/javascript/pwd.js
+++ assets/javascript/pwd.js
@@ -5,49 +5,49 @@ var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}'];
 
-
-window.onload = alert("Welcome! Please click 'Generate password' to start!");
-
 function generatePassword() {
     var length = prompt("enter the password length");
     console.log(length);
 
-    if (length < 6 || length >= 128) {
-        alert("The length of the password should be atleast 6 and less than 128 characters" + length);
+    if (length < 8 || length >= 128) {
+        alert("The length of the password should be atleast 8 and less than 128 characters");
     }
 
-    else if (length >= 6 || length < 128) {
+    else if (length >= 8 || length < 128) {
         var upperCaseEl = confirm("do you want upper case letters");
         var lowerCaseEl = confirm("do you want to have lowercase letters");
         var numbersEl = confirm("do you want to have numbers");
         var symbolsEl = confirm("do you want to have symbols");
-    }
-    var possibleChar = [];
-    var resultPassword = "";
 
-    if (upperCaseEl) {
-        Array.prototype.push.apply(possibleChar, upperCase);
-    }
-    console.log(possibleChar);
-    if (lowerCaseEl) {
-        Array.prototype.push.apply(possibleChar, lowerCase);
-    }
-    console.log(possibleChar);
-    if (numbersEl) {
-        Array.prototype.push.apply(possibleChar, numbers);
-    }
-    console.log(possibleChar);
-    if (symbolsEl) {
-        Array.prototype.push.apply(possibleChar, symbols);
-    }
-    console.log(possibleChar);
-    if (possibleChar.length === 0) {
-        alert("you should enter atleast 1 type of characters");
-    }
+        var possibleChar = [];
+        var resultPassword = "";
 
-    for (var i = 0; i < length; i++) {
-        var getRandom = Math.floor(Math.random() * possibleChar.length);
-        resultPassword = resultPassword + possibleChar[getRandom];
+        if (upperCaseEl) {
+            Array.prototype.push.apply(possibleChar, upperCase);
+        }
+        console.log(possibleChar);
+        if (lowerCaseEl) {
+            Array.prototype.push.apply(possibleChar, lowerCase);
+        }
+        console.log(possibleChar);
+        if (numbersEl) {
+            Array.prototype.push.apply(possibleChar, numbers);
+        }
+        console.log(possibleChar);
+        if (symbolsEl) {
+            Array.prototype.push.apply(possibleChar, symbols);
+        }
+        console.log(possibleChar);
+
+        if (possibleChar.length === 0) {
+            alert("you should enter atleast 1 type of characters");
+        }
+        else {
+            for (var i = 0; i < length; i++) {
+                var getRandom = possibleChar[Math.floor(Math.random() * possibleChar.length)];
+                resultPassword = resultPassword + getRandom;
+            }
+        }
     }
     console.log(resultPassword);
 
